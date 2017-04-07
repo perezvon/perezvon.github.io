0da91f47e4faec1ee2a16eaea2c392632ca44150
John A Maloney <johnamaloney@Johns-MacBook-Pro.local>
2017-04-07 11:37:14 -0500

    video hero

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ $(document).ready(function() {
     }
   });
 });
+
+
+var vid = document.getElementById("hero-video");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
